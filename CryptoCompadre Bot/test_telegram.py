3c42528538775ad17c67e8464ec8713b1be13cfa
Juan Diego Yepes <jyepes023@soyudemedellin.edu.co>
2025-04-08 09:35:31 -0500

    Reimplementacion Clase Bot

--- CryptoCompadre Bot/test_telegram.py
+++ CryptoCompadre Bot/test_telegram.py
@@ -1,31 +1,9 @@
 from config import * #importamos el token
 import telebot #para manejar la API de Telegram
 
+class CryptoCompadre_Bot:
 
-bot = telebot.TeleBot(TELEGRAM_TOKEN)
+    def __init__(self, TELEGRAM_TOKEN):
+        self.bot = telebot.TeleBot(TELEGRAM_TOKEN)
+        self.responder_comando()
 
-@bot.message_handler(commands=["start", "inicio"])        #responde al comando start
-def Responder_a_Comandos(message):
-    # El bot le da la bienvenida al usuario
-    bot.reply_to(message, "Bienvenido a CryptoCompadre, ¿Cómo puedo ayudarte?")
-
-
-@bot.message_handler(content_types=["text"])
-def responder_mensajes_texto(message):
-    if message.text.startswith("/"):
-        bot.send_message(message.chat.id, "Comando incorrecto mi compa ")
-
-    else:
-        bot.send_message(message.chat.id, "Recuerda, compadre, solo me comunico con comandos,"
-                                       " así que no entiendo lo que dices. ¡Intenta con un comando!")
-
-
-
-
-    #el atributo que recibe este objeto message es para registrar
-    # el id unico del chat de telegram con el usuario.
-    # El otro parámetro es el mensaje que responderá el bot a los mensajes de texto del usuario que no sean comandos.
-if __name__ == '__main__':
-    print('iniciando el bot')
-    bot.infinity_polling()
-    print('Fin')
