88fa269e9c5722bc8385b0a3698b09e0563f4a7c
Juan Diego Yepes <jyepes023@soyudemedellin.edu.co>
2025-04-08 09:45:23 -0500

    Reimplementacion Clase Bot

--- CryptoCompadre Bot/test_telegram.py
+++ CryptoCompadre Bot/test_telegram.py
@@ -7,3 +7,11 @@ class CryptoCompadre_Bot:
         self.bot = telebot.TeleBot(TELEGRAM_TOKEN)
         self.responder_comando()
 
+    def responder_comando(self):
+        @self.bot.message_handler(commands=["start", "inicio"])
+        def responder_a_comandos(message):
+            self.bienvenida_a_usuario(message)
+
+
+
+
