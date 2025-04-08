69d6993539c52d878238df942a3eff785640951d
Juan José Cuervo <juanjosecu234@gmail.com>
2025-04-07 22:05:09 -0500

    Dejaré la implementación inicial sin el uso de clases, ya que no sabía como usarlas sin cometer errores en los comandos.

--- CryptoCompadre Bot/test_telegram.py
+++ CryptoCompadre Bot/test_telegram.py
@@ -1,8 +1,15 @@
 from config import * #importamos el token
 import telebot #para manejar la API de Telegram
 
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
-class Bot:
-    def __init__(self, TELEGRAM_TOKEN):
-        self.bot = telebot.TeleBot(TELEGRAM_TOKEN)
+@bot.message_handler(commands=["start", "inicio"])        #responde al comando start
+def Responder_a_Start(message):
+    # El bot le da la bienvenida al usuario
+    bot.reply_to(message, "Bienvenido a CryptoCompadre, ¿Cómo puedo ayudarte?")
 
+
+if __name__ == '__main__':
+    print('iniciando el bot')
+    bot.infinity_polling()
+    print('Fin')
